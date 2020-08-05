86baad779cca80a427d7cd777746841e48a3c919
Bipal Goyal <56041473+Bipal123@users.noreply.github.com>
2020-08-05 22:02:58 +0545

    Create EventsCenter.js

---
+++ Themes/Jungle/EventsCenter.js
@@ -0,0 +1,42 @@
+import Phaser from 'phaser'
+
+const eventsCenter = new Phaser.Events.EventEmitter()
+
+export default eventsCenter
+
+import Phaser from 'phaser'
+
+export default class UIScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('ui-scene')
+		
+		// don't forget to import eventsCenter in the UIScene
+		import eventsCenter from './EventsCenter'
+	}
+
+
+	create()
+	{
+        this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "Score: 0", 16);
+        
+		//this.scoreLabel = this.add.text(10, 10, 'Count: 0', {
+		//	fontSize: 32
+		//})
+
+		// listen to 'update-count' event and call `updateCount()`
+		// when it fires
+		eventsCenter.on('update-count', this.updateCount, this)
+
+		// clean up when Scene is shutdown
+		this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
+			eventsCenter.off('update-count', this.updateCount, this)
+		})
+	}
+
+	updateCount(score)
+	{
+		this.scoreLabel.text = `Score: ${score}`
+	}
+}
